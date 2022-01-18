1d5b032c766012b3047efeb62f5f3e9189c2a124
Brandon1 <bennieb@sheridancollege.ca>
2022-01-18 14:01:59 -0500

    revert

--- src/student/studentList.java
+++ src/student/studentList.java
@@ -13,7 +13,7 @@ package student;
 public class studentList {
     
 public static void main(String[] args) {
-Student[] studentList = new Student[4];
+Student[] studentList = new Student[3];
 studentList[0] = new Student ("s1","Ronak Sheth");
 studentList[1] = new Student ("s2","John Doe");
 studentList[2] = new Student ("s3","Abc");
